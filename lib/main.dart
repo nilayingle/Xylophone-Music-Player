cb1062eab07229177ed0866b9bd017a7abfae7a8
nilayingle <nilayingle18@gmail.com>
2020-06-18 09:56:20 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
